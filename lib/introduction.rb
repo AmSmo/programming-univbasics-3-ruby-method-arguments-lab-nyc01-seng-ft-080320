31566c041346cfd085cdd422c118f95bbb4dbef7
Adam Smolenski <adam.smolenski@gmail.com>
2020-07-18 15:00:13 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -10,11 +10,11 @@ def introduction(name)
 end
 
 def introduction_with_language(name, language)
-  puts "Hi my name is #{name} and I am learning to program #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 def introduction_with_language_optional(name, language="english")
-  puts "Hi my name is #{name} and I am learning to program #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 
